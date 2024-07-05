853346811a06fd7ba365b5396768782136d996b4
LycixRemix <chengyongliu@foxmail.com>
2024-07-05 20:40:02 +0800

    feat(docs): add doc

--- components/ViewLayout.tsx
+++ components/ViewLayout.tsx
@@ -1,3 +1,5 @@
+import BackToTop from "./BackToTop";
+
 interface Props {
   children: React.ReactNode;
 }
@@ -5,6 +7,7 @@ interface Props {
 export default function ViewLayout({ children }: Props) {
   return (
     <div className="bg-white max-w-4xl mx-auto min-h-screen md:border-solid md:border-[#ccc] md:border-[1px] md:shadow-[2px_2px_8px_#aaa] md:mt-[1.5em] md:mb-[.5em] py-[1.5em] px-[2em]">
+      <BackToTop />
       {children}
     </div>
   );
